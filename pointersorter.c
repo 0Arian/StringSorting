40806d16fe959c3a42544ab836ec1f469102e3a8
revoxx5 <amoslem678@gmail.com>
2017-01-26 14:41:41 -0500

    Added Base Project, currently it prints all the characters

--- pointersorter.c
+++ pointersorter.c
@@ -1,5 +1,14 @@
 /* 
  * Pointer Sorter - Assignment 0 Systems Programming(CS 214)
  * Created by Arian Moslem(Section #3) and Nicholas Knight(Section #3)
- * /
-
+ */
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char *argv[]){
+    int i;
+    char* c = argv[1];
+    for(i = 0; i<strlen(argv[1]); i++){
+        printf("%c\n", c[i]);
+    }
+    return 0;
+}
