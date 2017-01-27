d43791325b548587c2d375d44217d4ed9aa4f6ba
revoxx5 <amoslem678@gmail.com>
2017-01-27 13:44:55 -0500

    Updated code to check if it's a letter or not

--- pointersorter.c
+++ pointersorter.c
@@ -4,6 +4,7 @@
  */
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 typedef enum bool{true, false} bool;
 
@@ -12,7 +13,13 @@ int main(int argc, char *argv[]){
     int i; //Will be used to keep track of the characters in our for loop
     char* c = argv[1]; //Derefencing the given string
     for(i = 0; i<strlen(argv[1]); i++){
-        printf("%c\n", c[i]); //Printing out all characters that are inputted, while always pushing to a new line
+        if (isalpha(c[i])){
+            printf("%c\t is alpha\n", c[i]);
+        }
+        else {
+            printf("%c\t is not alpha\n", c[i]);
+        }
+        //printf("%c\n", c[i]); //Printing out all characters that are inputted, while always pushing to a new line
     }
 
     return 0;
