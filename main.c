62b7d1db214053177c318052e3398f7527dac857
NicholasKnight95 <knightn1@montclair.edu>
2017-01-29 15:20:52 -0500

    Brushed up the code, linked list now stores, and sorts properly. This needs to be tested thouroughly, and commented!

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ typedef struct node{
 
 node *head = NULL;
 
+//Checks for invalid input, such as not enough parameters, and a NULL input.
 int Input_Validator(int argc, char **argv){
     if(argc != 2){
         fprintf(stderr, "ERROR: Not enough parameters entered.\n");
@@ -20,6 +21,8 @@ int Input_Validator(int argc, char **argv){
 
     return 0;
 }
+//Gets rid of all the garbage delimiters in the input, and replaces them with a consistent
+// ' ' for clarity.
 char* GetRidOfShit(char *argv){
     int iterator = 0;
     char* copy = strdup(argv);
@@ -33,11 +36,27 @@ char* GetRidOfShit(char *argv){
     printf("CLEAN: %s\n", copy);
     return copy;
 }
+//Compares str1 to str2. If a negative number is returned, str1 is before str2
+//alphabetically. If a positive number is returned, str2 comes before str1 alphabetically.
+//If a 0 is returned, they are equal.
+int Compare(char* str1, char* str2){
+    int i;
+    for(i = 0; str1[i] == str2[i]; i++){
+        if (str1[i] == '\0'){
+            return 0;
+        } else if(str2[i] == '\0'){
+            return 1;
+        }
+    }
+    printf("NUM: %d\n", (str1[i] - str2[i]));
+    return str1[i] - str2[i];
+}
 
 void Sort(char *RD){
-    int i = 0, beg = 0, end = 0;
+    int i = 0, beg = 0, end = 0, cont = 0;
     node *ptr = NULL;
     char *copy = strdup(RD);
+    char* word = (char*) malloc(sizeof(char));
     printf("STRING: %s\n", RD);
     if(RD == NULL){
         fprintf(stderr, "No alphabetic characters detected.\n");
@@ -54,15 +73,69 @@ void Sort(char *RD){
         }
         end = i;
         if(head->data == NULL){
-            head->data = (char*) malloc(end-beg + 1);
+            head->data = (char*) malloc(sizeof(char) * (end-beg + 1));
             strncpy(head->data, copy + beg, end-beg);
             head->data[end-beg + 1] = '\0';
             ptr = head->next;
+            printf("WORD: %s\n", head->data);
+        } else{
+            word = malloc(sizeof(char) * (end-beg + 1));
+            strncpy(word, copy + beg, end-beg);
+            word[end-beg] = '\0';
+            ptr = head;
+            cont = 1;
+            printf("WORD: %s\n", word);
+            if(ptr->next == NULL){
+                if(Compare(word, ptr->data) > 0){
+                    ptr->next = malloc(sizeof(node) + strlen(word) + 1);
+                    ptr->next->data = (char*) malloc(sizeof(char) * strlen(word) + 1);
+                    ptr->next->data = word;
+                    ptr->next->next = NULL;
+                } else{
+                    char* temp = (char*) malloc(sizeof(char) * strlen(head->data) + 1);
+                    temp = head->data;
+                    head->data = (char*) malloc(sizeof(char) * strlen(word) + 1);
+                    head->data = word;
+                    head->next = malloc(sizeof(node) + strlen(word) + 1);
+                    head->next->data = (char*) malloc(sizeof(char) * strlen(temp) + 1);
+                    head->next->data = temp;
+                    head->next->next = NULL;
+                    printf("NEW-HEAD: %s; OLD-HEAD: %s\n", head->data, head->next->data);
+                }
+            }else{
+            while(ptr != NULL && cont == 1){
+                if(Compare(word, ptr->data) <= 0){
+                    ptr = malloc(sizeof(node) + strlen(word) + 1);
+                    ptr->data = (char*) malloc(sizeof(char) * strlen(word) + 1);
+                    ptr->data = word;
+                    ptr->next = head;
+                    head = ptr;
+                    cont = 0;
+                }else if(ptr->next == NULL){
+                    node *temp = NULL;
+                    ptr->next = malloc(sizeof(node) + strlen(word) + 1);
+                    ptr->next->data = (char*) malloc(sizeof(char) * strlen(word) + 1);
+                    ptr->next->data = word;
+                    ptr->next->next = temp;
+                    cont = 0;
+                }else if(Compare(word, ptr->data) > 0 && (Compare(word, ptr->next->data) <= 0)){
+                    node *temp = ptr->next;
+                    ptr->next = malloc(sizeof(node) + strlen(word) + 1);
+                    ptr->next->data = (char*) malloc(sizeof(char) * strlen(word) + 1);
+                    ptr->next->data = word;
+                    ptr->next->next = temp;
+                    cont = 0;
+                }else{
+                    ptr = ptr->next;
+                }
+            }
+            }
         }
-        printf("HERE: %s\n", head->data);
-
-        printf("Value: %d\n", RD[i]);
         i++;
+    }
+    while(head != NULL){
+        printf("%s\n", head->data);
+        head = head->next;
     }
     return;
 }
